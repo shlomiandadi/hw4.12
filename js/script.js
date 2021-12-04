060fb8fa4e65bc4950fe187a7017d91def2c7cff
shlomi <you@example.com>
2021-12-04 18:40:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,107 @@
+var num1,num2;
+num1=+prompt("plese enter num 1:");
+num2=+prompt("plese enter num 2:");
+if (num1>num2) {
+    alert( "num1="+" "+num1 +" "+ "number one bigeer is number two")
+}
+else if(num1=num2)
+{
+    alert("number="+" "+num1)
+}
+else 
+{
+    alert("num 2 biiger")
+}5
+// 
+var sacsess
+sacsess=prompt("plese enter sacsess:")
+if(sacsess>=60)
+{
+    alert("עובר")
+}
+else
+{
+    alert("נכשל")
+}
+// 
+var num
+num=+prompt("plese enter num :");
+if(num>0)
+{
+    alert("חיובי")
+}
+else if(num<0)
+{
+    alert("שלילי")
+}
+else
+{
+    alert("אפס")
+}
+// 
+var num
+num=prompt("plese enter num :");
+if(num<0)
+{
+    alert("המספר הינו מספר שלילי")
+}
+else if(num<=0)
+{
+    alert(	"המספר הינו אפס")
+}
+else if(num>=1 && num<=100 )
+{
+    alert("המספר הינו בין 1 ל-100 כולל")
+   
+}
+else if(num>=101 && num<=1000)
+{
+    alert("המספר גדול מ-100")
+}
+
+
+else
+{
+    alert("המספר גדול מ1000")
+}
+// 
+var Score
+Score=prompt("plese enter Score :");
+if(Score<=59)
+{
+    alert("נכשל")
+}
+else if(Score<=69)
+{
+    alert(	"מספיק")
+}
+else if(Score<=79 )
+{
+    alert("כמעט טוב")
+   
+}
+else if(Score<=89)
+{
+    alert("טוב")
+}
+else if(Score<=99)
+{
+    alert("טוב מאוד")
+}
+
+else
+{
+    alert("מעולה")
+}
+// 
+var age
+age= prompt("plese enter age:")
+if(age>=17)
+{
+    alert("אתה רשאי להוציא רשיון נהיגה")
+}
+else
+{
+    alert("גילך צעיר מדי")
+}
+
